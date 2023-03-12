b13ffb8941382a4e342058ee4d09486dd3f16d93
unknown <vigneshvikky199923@gmail.com>
2023-03-12 23:50:56 +0530

    third

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,7 +11,8 @@ export class LoginComponent {
   //data1 = "Your Perfect Banking Partner" //   DB(1) databinding-----string interpolation //we stote data directly so it no need to define declaration
 data2="Enter A/C number"  //DB(2) propertybinding
 
-
+acno:any
+psw:any
 
 
 
@@ -27,37 +28,23 @@ data2="Enter A/C number"  //DB(2) propertybinding
 
   }
 
-//  login(){
-//   var acnum=this.acno
-//   var psw=this.psw
-//   var userDetails=this.userDetails
-//   if(acnum in userDetails){
-//       if(psw==userDetails[acnum]["password"]){   //if we want to callkey ,key must inside the quotes but acno is a variable
-//       alert('login success')
-//       }else{
-//         alert('incurrect password')
-//       }
-//   }else{
-//     alert('incurrect account number')
-//   }
-//  }
+ login(){
+  var acnum=this.acno
+  var psw=this.psw
+  var userDetails=this.userDetails
+  if(acnum in userDetails){
+      if(psw==userDetails[acnum]["password"]){   //if we want to callkey ,key must inside the quotes but acno is a variable
+      alert('login success')
+      }else{
+        alert('incurrect password')
+      }
+  }else{
+    alert('incurrect account number')
+  }
+ }
 
 
 
 
-   login(acnum:any,psw:any){
-    var acnum=acnum.value
-    var psw=psw.value    
-    var userDetails=this.userDetails
-    if(acnum in userDetails){
-        if(psw==userDetails[acnum]["password"]){   //if we want to callkey ,key must inside the quotes but acno is a variable
-        alert('login success')
-        }else{
-          alert('incurrect password')
-        }
-    }else{
-      alert('incurrect account number')
-    }
-   }
 
 }
